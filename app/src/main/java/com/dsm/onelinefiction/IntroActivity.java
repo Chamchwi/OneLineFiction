b6db8ed3a071b4c7f850cc2f9bdd978c6e30d7eb
SpiralMoon <mjh1660@gmail.com>
2017-03-28 14:10:35 +0900

    20170328 0.0.1 ver

---
+++ app/src/main/java/com/dsm/onelinefiction/IntroActivity.java
@@ -0,0 +1,35 @@
+package com.dsm.onelinefiction;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private int count = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                if (count != 0)
+                    count--;
+                else {
+                    Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    cancel();
+                    finish();
+                }
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(timerTask, 0, 1000);
+    }
+}
