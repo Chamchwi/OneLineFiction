c6e6ab29448bd053aa08b6399f514d1148c933b7
SpiralMoon <mjh1660@gmail.com>
2017-03-29 14:06:38 +0900

    20170329 0.0.3 ver

--- app/src/main/java/com/dsm/onelinefiction/LoginActivity.java
+++ app/src/main/java/com/dsm/onelinefiction/LoginActivity.java
@@ -2,26 +2,43 @@ package com.dsm.onelinefiction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.telecom.Connection;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private SignInButton BTN_Login;
     private GoogleApiClient googleApiClient;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        BTN_Login = (SignInButton) findViewById(R.id.btn_login);
+        firebaseAuth = FirebaseAuth.getInstance();
+
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.))
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
@@ -43,7 +60,33 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 100) {
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            GoogleSignInAccount googleSignInAccount = googleSignInResult.getSignInAccount();
 
+            if(googleSignInResult.isSuccess())
+                firebaseWithGoogle(googleSignInAccount);
+            else
+                Toast.makeText(this, "인증에 실패하였습니다.", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public void onConnectionFailed (@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "인증에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+    }
+
+    private void firebaseWithGoogle (GoogleSignInAccount googleSignInAccount) {
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
+
+        Task<AuthResult> authResults = firebaseAuth.signInWithCredential(authCredential);
+
+        authResults.addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                FirebaseUser firebaseUser = authResult.getUser();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
