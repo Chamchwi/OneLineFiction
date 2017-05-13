2f54ca45854f41f09410af5f799dc5261b9d83b1
SpiralMoon <mjh1660@gmail.com>
2017-05-13 16:05:24 +0900

    20170513 0.1.1 ver

--- app/src/main/java/com/dsm/onelinefiction/MainActivity.java
+++ app/src/main/java/com/dsm/onelinefiction/MainActivity.java
@@ -3,6 +3,7 @@ package com.dsm.onelinefiction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private DrawerLayout navigationDrawer;
     private ArrayList<Item> dataSet;
     private Database database;
     private FloatingActionButton btnAdd;
@@ -42,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         recyclerView.setHasFixedSize(true);
         progressBar = (ProgressBar) findViewById(R.id.prg);
+        navigationDrawer = (DrawerLayout) findViewById(R.id.view_main);
 
 //        layoutManager = new LinearLayoutManager(this);
 //        layoutManager = new GridLayoutManager(this, 3);
@@ -65,5 +68,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Toast.makeText(this, "dd", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
